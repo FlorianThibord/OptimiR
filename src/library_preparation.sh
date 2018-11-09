63c46804eec1ba9b72dbf2fe663baade68654b16
FlorianThibord <florian.thibord@gmail.com>
2018-11-09 15:04:34 +0100

    R_plot scripts added

--- src/library_preparation.sh
+++
@@ -1,6 +0,0 @@
-python library_preparation.py \
-       -v ../ressources/Library_prep/MARTHA_good_rsq.vcf \
-       -m ../ressources/Library_prep/resources/fasta/hsa_matures_miRBase_v21.fa \
-       -p ../ressources/Library_prep/resources/fasta/hsa_hairpins_miRBase_v21.fa \
-       -g ../ressources/Library_prep/resources/coordinates/hsa_miRBase_v21.gff3 \
-       -o ./OptimiR_Files/
